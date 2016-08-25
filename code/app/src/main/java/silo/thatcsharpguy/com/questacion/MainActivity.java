dc304d80681d63e7a0893831fce84c00a6acf1a7
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-08-25 17:00:46 -0500

    Download databasefile.

--- code/app/src/main/java/silo/thatcsharpguy/com/questacion/MainActivity.java
+++ code/app/src/main/java/silo/thatcsharpguy/com/questacion/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
@@ -19,11 +20,22 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import silo.thatcsharpguy.com.questacion.services.DownloadTask;
 import silo.thatcsharpguy.com.questacion.services.LocationService;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int MY_PERMISSIONS_REQUEST_LOCATION = 245;
+    private static final int MY_PERMISSIONS_STORAGE = 12354;
 
     LocationService mService;
     boolean mBound = false;
@@ -36,20 +48,41 @@ public class MainActivity extends AppCompatActivity {
         Button start =(Button ) findViewById(R.id.startTrip);
 
 
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            Log.e("LOCATION SERVICE", "NOT ACCEPTED");
-            return;
-        }
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if(
+                        (ContextCompat.checkSelfPermission(MainActivity.this,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                                != PackageManager.PERMISSION_GRANTED)&&
+                                (ContextCompat.checkSelfPermission(MainActivity.this,
+                                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                                        != PackageManager.PERMISSION_GRANTED))
+                {
+                    Log.w("MainActivity", "Sin permisos");
+
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{
+                                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                            },
+                            MY_PERMISSIONS_STORAGE);
+                }else
+                {
+                   startDownloadingDatabase();
+
+
+                }
+            }
+        });
     }
 
+    void startDownloadingDatabase(){
+        final DownloadTask downloadTask = new DownloadTask(MainActivity.this);
+         downloadTask.execute("https://github.com/fferegrino/questacion/blob/master/sqlite/mb.sqlite?raw=true");
+    }
 
     /** Defines callbacks for service binding, passed to bindService() */
     private ServiceConnection mConnection = new ServiceConnection() {
@@ -73,14 +106,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
 
 
-
         // Here, thisActivity is the current activity
         if ((ContextCompat.checkSelfPermission(this,
                 android.Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) &&
                 (ContextCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) ) {
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) ) {
 
             // Should we show an explanation?
             boolean t = ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.ACCESS_FINE_LOCATION);
@@ -97,7 +129,9 @@ public class MainActivity extends AppCompatActivity {
                 ActivityCompat.requestPermissions(this,
                         new String[]{
                                 android.Manifest.permission.ACCESS_FINE_LOCATION,
-                                Manifest.permission.ACCESS_COARSE_LOCATION
+                                Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.INTERNET,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE
                         },
                         MY_PERMISSIONS_REQUEST_LOCATION);
 
@@ -105,14 +139,17 @@ public class MainActivity extends AppCompatActivity {
                 // app-defined int constant. The callback method gets the
                 // result of the request.
             }
-        }else
+        }
+
+/*
+        else
         {
             Log.i("LOCATION SERVICE", "Has permissions");
             launchService();
         }
+*/
         super.onStart();
         // Bind to LocalService
-
     }
 
     void launchService(){
@@ -126,23 +163,12 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
         switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_LOCATION: {
+            case MY_PERMISSIONS_STORAGE: {
                 // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    launchService();
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startDownloadingDatabase();
                 }
-                return;
             }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
         }
     }
 
