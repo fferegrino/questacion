e291c857708ab4d4830cc6af0466df845ecc00e1
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-08-27 07:36:19 -0500

    Actividades separadas para permisos y principal.

--- code/app/src/main/java/silo/thatcsharpguy/com/questacion/MainActivity.java
+++ code/app/src/main/java/silo/thatcsharpguy/com/questacion/MainActivity.java
@@ -1,41 +1,27 @@
 package silo.thatcsharpguy.com.questacion;
 
-import android.Manifest;
-import android.app.ActionBar;
 import android.content.ComponentName;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.location.Location;
-import android.net.Uri;
+import android.os.Bundle;
 import android.os.IBinder;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
-
-import java.util.List;
 
 import jsqlite.Exception;
 import silo.thatcsharpguy.com.questacion.dataaccess.MetrobusDatabase;
@@ -48,15 +34,6 @@ public class MainActivity extends AppCompatActivity
         implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, NuevaEstacionListener{
 
     private static final String TAG = "MainActivity";
-
-    private static final String WriteStorage = android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
-    private static final String ReadStorage = android.Manifest.permission.READ_EXTERNAL_STORAGE;
-
-    private static final String AccessCoarseLocation = Manifest.permission.ACCESS_COARSE_LOCATION;
-    private static final String AccessFineLocation = Manifest.permission.ACCESS_FINE_LOCATION;
-
-    private static final int LOCATION_REQUEST = 2;
-    private static final int STORAGE_REQUEST = 3;
 
     LocationService mService;
     boolean mBound = false;
@@ -74,7 +51,9 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         _dialogBuilder = new AlertDialog.Builder(this);
+
         _estacionCercanaText = (TextView)findViewById(R.id.estacionCercanaText);
         _mainPanel = (LinearLayout)findViewById(R.id.mainPanel);
         _bindUnbindButton = (Button)findViewById(R.id.bindUnbindButton);
@@ -96,8 +75,54 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        int color = savedInstanceState.getInt("color");
+        _mainPanel.getRootView().setBackgroundColor(color);
+
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt("color", 0xFF990000);
+
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
     protected void onStart() {
-        checkDatabase();
+
+
+        if(Commons.getMainDatabaseFile().exists())
+        {
+            try {
+                MetrobusDatabase = new MetrobusDatabase(Commons.getMainDatabaseFile());
+
+                if (GoogleApiClient == null) {
+                    GoogleApiClient = new GoogleApiClient.Builder(this)
+                            .addConnectionCallbacks(this)
+                            .addOnConnectionFailedListener(this)
+                            .addApi(LocationServices.API)
+                            .build();
+                }
+
+                if(!GoogleApiClient.isConnected()) {
+                    GoogleApiClient.connect();
+                }else{
+                    StartAndBindLocationService();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        else
+        {
+            Intent intent = new Intent(this, DownloadDatabaseActivity.class);
+            startActivity(intent);
+        }
+
         super.onStart();
     }
 
@@ -107,205 +132,15 @@ public class MainActivity extends AppCompatActivity
         super.onDestroy();
     }
 
-    private void checkDatabase(){
-        // Check for permissions
-        int canReadStorage = ContextCompat.checkSelfPermission(this,ReadStorage);
-        int canWriteStorage = ContextCompat.checkSelfPermission(this,WriteStorage);
-
-        if(canReadStorage == PackageManager.PERMISSION_GRANTED &&
-                canWriteStorage == PackageManager.PERMISSION_GRANTED )
-        {
-            if(Commons.getMainDatabaseFile().exists())
-            {
-                try {
-
-                    MetrobusDatabase = new MetrobusDatabase(Commons.getMainDatabaseFile());
-
-                    setupLocationService();
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-            }
-            else
-            {
-                Log.w(TAG,"No database yet");
-                Intent intent = new Intent(this, DownloadDatabaseActivity.class);
-                startActivity(intent);
-            }
-        }
-        else
-        {
-            requestStoragePermissions();
-        }
-    }
-
-
-    private void setupLocationService(){
-        // Check for permissions
-        int canReadStorage = ContextCompat.checkSelfPermission(this,AccessFineLocation);
-        int canWriteStorage = ContextCompat.checkSelfPermission(this,AccessCoarseLocation);
-
-        if(canReadStorage == PackageManager.PERMISSION_GRANTED &&
-                canWriteStorage == PackageManager.PERMISSION_GRANTED )
-        {
-            // Start service or whatever
-            Log.i(TAG,"Ok location permissions");
-
-            if (GoogleApiClient == null) {
-                GoogleApiClient = new GoogleApiClient.Builder(this)
-                        .addConnectionCallbacks(this)
-                        .addOnConnectionFailedListener(this)
-                        .addApi(LocationServices.API)
-                        .build();
-            }
-            GoogleApiClient.connect();
-        }
-        else
-        {
-            requestLocationPermissions();
-        }
-    }
-
-    private void requestStoragePermissions(){
-        // Should we show an explanation?
-        boolean requiresRational = ActivityCompat.shouldShowRequestPermissionRationale(this,ReadStorage) ||
-                ActivityCompat.shouldShowRequestPermissionRationale(this,WriteStorage);
-
-        if (!requiresRational) {
-            AlertDialog dialog = _dialogBuilder.setTitle("Necesitamos tu permiso")
-                    .setMessage(R.string.database_explanation)
-                    .setPositiveButton(R.string.ok_open_settings, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            startInstalledAppDetailsActivity();
-                        }
-                    })
-                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            Log.e(TAG,"DUDE WHAT");
-                        }
-                    }).create();
-
-            dialog.show();
-        } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{
-                            ReadStorage,
-                            WriteStorage
-                    },
-                    STORAGE_REQUEST);
-        }
-    }
-
-    private void requestLocationPermissions(){
-        // Should we show an explanation?
-        boolean requiresRational = ActivityCompat.shouldShowRequestPermissionRationale(this,AccessCoarseLocation) ||
-                ActivityCompat.shouldShowRequestPermissionRationale(this,AccessFineLocation);
-
-        if (!requiresRational) {
-
-
-            AlertDialog dialog = _dialogBuilder.setTitle("Necesitamos tu permiso")
-                    .setMessage(R.string.location_explanation)
-                    .setPositiveButton(R.string.ok_open_settings, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            startInstalledAppDetailsActivity();
-                        }
-                    })
-                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            Log.e(TAG,"DUDE WHAT");
-                        }
-                    }).create();
-
-            dialog.show();
-        } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{
-                            AccessCoarseLocation,
-                            AccessFineLocation
-                    },
-                    LOCATION_REQUEST);
-        }
-    }
-
-    public void startInstalledAppDetailsActivity() {
-
-        final Intent i = new Intent();
-        i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        i.addCategory(Intent.CATEGORY_DEFAULT);
-        i.setData(Uri.parse("package:" + getPackageName()));
-        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-        i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-        startActivity(i);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case STORAGE_REQUEST: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    checkDatabase();
-                }
-                else{
-
-                    AlertDialog dialog = _dialogBuilder.setTitle("Necesitamos tu permiso")
-                            .setMessage(R.string.database_explanation)
-                            .setPositiveButton(R.string.ok_retry, new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
-                                    requestStoragePermissions();
-                                }
-                            })
-                            .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
-                                    Log.e(TAG,"DUDE WHAT");
-                                }
-                            }).create();
-
-                    dialog.show();
-                }
-            }
-            case LOCATION_REQUEST: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    setupLocationService();
-                }
-                else{
-
-                    AlertDialog dialog = _dialogBuilder.setTitle("Necesitamos tu permiso")
-                            .setMessage(R.string.location_explanation)
-                            .setPositiveButton(R.string.ok_retry, new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
-                                    requestLocationPermissions();
-                                }
-                            })
-                            .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
-                                    Log.e(TAG,"DUDE WHAT");
-                                }
-                            }).create();
-
-                    dialog.show();
-                }
-            }
-        }
-    }
-
     Intent locationServiceIntent;
 
     void StartAndBindLocationService(){
-        Log.i(TAG, "Start the service");
         locationServiceIntent = new Intent(this, LocationService.class);
         startService(locationServiceIntent);
         bindService(locationServiceIntent, mConnection, Context.BIND_AUTO_CREATE);
     }
 
     void StopAndUnbindLocationService(){
-        Log.i(TAG, "Stop the service");
         if(mBound) {
             stopService(locationServiceIntent);
             unbindService(mConnection);
@@ -314,6 +149,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    @SuppressWarnings({"MissingPermission"})
     public void onConnected(@Nullable Bundle bundle) {
 
         Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(GoogleApiClient);
@@ -345,6 +181,7 @@ public class MainActivity extends AppCompatActivity
 
         @Override
         public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
         }
     };
 
@@ -360,32 +197,23 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void stationUpdate(Estacion cercana) {
+
+        int color = 0xFF000000 | cercana.getColor();
+
         _estacionCercanaText.setText(cercana.getNombre());
         _estacionCercanaText.setTextColor(Color.WHITE);
-        int color = 0xFF000000 | cercana.getColor();
         _mainPanel.getRootView().setBackgroundColor(color);
 
-        float[] hsv = new float[3];
-        Color.colorToHSV(color, hsv);
-        hsv[2] *= 0.8f; // value component
-        color = Color.HSVToColor(hsv);
+        color = Utils.darkenColor(color,0.8f);
 
         android.support.v7.app.ActionBar supportActionBar = getSupportActionBar();
 
         supportActionBar.setBackgroundDrawable(new ColorDrawable(color));
+        supportActionBar.setWindowTitle("Linea " + cercana.getLinea());
 
-        Window window = this.getWindow();
-
-// clear FLAG_TRANSLUCENT_STATUS flag:
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
-// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-
-        hsv[2] *= 0.8f; // value component
-        color = Color.HSVToColor(hsv);
-// finally change the color
-        window.setStatusBarColor(color);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        getWindow().setStatusBarColor(Utils.darkenColor(color, 0.8f));
 
     }
 }
