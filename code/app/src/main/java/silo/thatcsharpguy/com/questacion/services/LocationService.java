582dcec2225028404d1e8a93def834248cea55e3
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-08-27 07:35:19 -0500

    Limpieza :sweat:

--- code/app/src/main/java/silo/thatcsharpguy/com/questacion/services/LocationService.java
+++ code/app/src/main/java/silo/thatcsharpguy/com/questacion/services/LocationService.java
@@ -1,28 +1,18 @@
 package silo.thatcsharpguy.com.questacion.services;
 
-import android.Manifest;
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.location.Location;
-import android.location.LocationListener;
 import android.os.Binder;
-import android.os.Bundle;
 import android.os.IBinder;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
@@ -34,7 +24,6 @@ import silo.thatcsharpguy.com.questacion.Commons;
 import silo.thatcsharpguy.com.questacion.MainActivity;
 import silo.thatcsharpguy.com.questacion.R;
 import silo.thatcsharpguy.com.questacion.entities.Estacion;
-import silo.thatcsharpguy.com.questacion.entities.HardcodedData;
 
 /**
  * Created by anton on 8/24/2016.
@@ -61,6 +50,7 @@ public class LocationService
 
     @Nullable
     @Override
+    @SuppressWarnings({"MissingPermission"})
     public IBinder onBind(Intent intent) {
 
         Log.i("LOCATION SERVICE", "onBind");
